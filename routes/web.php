a8611507e7c495d791f8951b666f14a1297fcc05
Gustavo Barros da Silveira <guga082011@gmail.com>
2024-06-30 15:34:25 -0300

    Adicionar rotas para as linguagens

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/{locale}', function ($locale) {
+    if (isset($locale)){
+        App::setLocale($locale);
+    }    
+    return view('welcome');
+})->name('home');
+
 Route::get('/', function () {
+    App::setLocale("br");
     return view('welcome');
 });
